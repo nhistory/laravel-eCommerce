836b5d73e9880c5e67fd77e786fb4cecc57b74cb
nhistory <nhistory@naver.com>
2022-04-05 00:35:24 -0300

    Main page bootstrap setup

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,4 +1,19 @@
 @extends('master')
 @section('content')
-<h1>Login page</h1>
-<button class="btn btn-primary">Click on me</button>
+<div class="container">
+    <div class="row">
+        <form class="mx-auto">
+            <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+</div>
+@endsection
